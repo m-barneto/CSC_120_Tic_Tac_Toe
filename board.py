fb321486c47a6ec05fdd36696989a6bccaad5aa0
mbarneto <mattdokn@gmail.com>
2022-03-25 12:44:01 -0400

    Stage 1: View the board

---
+++ board.py
@@ -0,0 +1,11 @@
+def print_board(board):
+  for row in range(len(board)):
+    print(board[row])
+
+
+def main():
+  board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+  print_board(board)
+
+
+main()
